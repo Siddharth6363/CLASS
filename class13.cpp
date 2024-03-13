5930b2fd6234b7b855806ebdbc7d71375b0d6925
Siddharth Chauhan <siddharthc443@gmail.com>
2024-03-13 22:15:23 +0530

    first

---
+++ class13.cpp
@@ -0,0 +1,53 @@
+#include<iostream>
+#include<ctime>
+using namespace std;
+
+class Random{
+    public:
+    int num;
+    int tries;
+    int guess;
+
+    void game();
+};
+
+ void Random::game()
+    {
+             srand(time(NULL));   //random number generator 
+                    num=(rand() % 100)+1;
+
+        cout<<"*******NUMBER GUESS GAME TEST UR LUCK********\n";
+
+                do{
+                    cout<<"Guess a number between (1-100):";
+                        cin>>guess;
+                        tries++;
+
+                        if(guess>num)
+                        {
+                            cout<<"Number is lower than your guess\n";
+                        }
+
+                        else if(guess<num)
+                            {
+                            cout<<"Number is higher than your guess\n";
+                            }
+
+                            else{
+                                cout<<"Correct!!!# of tries: "<<tries<<'\n';
+                                }
+
+                }while (guess !=num);
+                    cout<<"********THE GAME ENDS HERE********"; 
+    }
+
+
+
+int main()
+{   Random obj1;
+        obj1.game();
+
+      
+
+return 0;
+}
